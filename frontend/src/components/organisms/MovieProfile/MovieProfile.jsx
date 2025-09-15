d9707fdde88468b95702507b3d47916b2958d050
Max Rebhorn <M.Rebhorn@queo-group.com>
2025-09-15 13:56:27 +0200

    PQWEBAZUBI-125 Film Fragebogen
    Patched up the styling a bit

--- frontend/src/components/organisms/MovieProfile/MovieProfile.jsx
+++ frontend/src/components/organisms/MovieProfile/MovieProfile.jsx
@@ -68,7 +68,8 @@ function MovieProfile({
     };
 
     // prepare icon names based on theme
-    const reviewIcon = theme === 'light' ? 'review_light.svg' :'review.svg' ;
+    const reportIcon = theme === 'light' ? 'review_light.svg' :'review.svg' ;
+    const reviewIcon = theme === 'light' ? 'report_light.svg' :'report.svg' ;
     const favoriteIcon = isFavorite
         ? (theme === 'light' ? 'like_filled_light.svg' : 'like_filled.svg')
         : (theme === 'light' ? 'like_light.svg' : 'like.svg');
@@ -89,6 +90,11 @@ function MovieProfile({
                         onClick={handleFavoriteClick}
                         disabled={loading}
                         name={favoriteIcon}
+                    />
+                    <IconButton
+                        onClick={handleFavoriteClick}
+                        disabled={loading}
+                        name={reportIcon}
                     />
                 </div>
             </div>
